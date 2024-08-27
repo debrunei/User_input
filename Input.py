82324012025578b9d5cb77b12e45e4920dea1ed6
debrunei000 <debrunei000@warren.k12.in.us>
2024-08-27 10:06:22 -0400

    First commit

---
+++ Input.py
@@ -0,0 +1,4 @@
+Name = input("Enter your name: ")
+Droids = input("How many droids do you want to meet: ")
+wookiees = input("How many wookiees do you want to meet: ")
+print(Name, "wants to meet",Droids, "droids and", wookiees, "Wookiees")
